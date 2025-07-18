40cb5d291a1698d8947a7fd0008bb03012f90f1f
Dr Herilala <hei.teddy.3@gmail.com>
2025-07-18 19:20:39 +0300

    style: toggle Switch

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -20,7 +20,14 @@ const DarkModeToggle = () => {
       html.classList.remove('dark');
       localStorage.setItem('theme', 'light');
     }
+    
   }, [isDarkMode]); 
+  const [LightMode, setBoolean] = useState(false)
+  //manao boolean accessible dans tout le projet pour un theme switch
+  //AZA ADINO
+  
+  
+  
 
   const toggleDarkMode = () => {
     setIsDarkMode(prevMode => !prevMode);
@@ -32,13 +39,12 @@ const DarkModeToggle = () => {
       className={`
         p-2 rounded-full flex items-center justify-center
         transition-colors duration-300 ease-in-out
-        ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-yellow-300 text-gray-900'}
+        ${isDarkMode ? 'bg-gray-800 text-black' : 'bg-gray-300 text-gray-950'}
         shadow-md focus:outline-none focus:ring-2 focus:ring-opacity-75
-        ${isDarkMode ? 'focus:ring-gray-700' : 'focus:ring-yellow-400'}
-      `}
-      aria-label={isDarkMode ? 'Passer en mode clair' : 'Passer en mode sombre'}
+        ${isDarkMode ? 'focus:ring-gray-400' : 'focus:ring-gary-400'}
+      `} 
     >
-      {isDarkMode ? (<Sun/>) : (<Moon/>)}
+      {isDarkMode ? (<Moon/>) : (<Sun/>)}
     </button>
   );
 };
